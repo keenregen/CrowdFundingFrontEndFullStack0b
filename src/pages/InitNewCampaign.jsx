20a727ece216e08d1afed7afd5a00287a7d53073
keenregen <keenregen@gmail.com>
2023-02-12 06:04:21 +0300

    v1.0

---
+++ src/pages/InitNewCampaign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InıtNewCampaign = () => {
+  return (
+    <div>InıtNewCampaign</div>
+  )
+}
+
+export default InıtNewCampaign
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as InitNewCampaign } from './InitNewCampaign';
+export { default as CampaignDetails } from './CampaignDetails';
